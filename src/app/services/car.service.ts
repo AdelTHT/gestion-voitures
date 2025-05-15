76e39ca1c530ced5a40b2cbb5480b01548439cd1
AdelTHT <tahanoutadel@gmail.com>
2025-05-15 13:56:25 +0200

    Update car.service.ts

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,50 +1,38 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Car } from '../models/car.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
-  // Simuler les données des voitures
-  private cars: Car[] = [
-    { id: 1, marque: 'Toyota', modele: 'Corolla', couleur: 'bleu', image: 'https://example.com/toyota-corolla.jpg' },
-    { id: 2, marque: 'Honda', modele: 'Civic', couleur: 'rouge', image: 'https://example.com/honda-civic.jpg' },
-    { id: 3, marque: 'Ford', modele: 'Focus', couleur: 'noir',  image: 'https://example.com/ford-focus.jpg' },
-  ];
+  private apiUrl = 'http://localhost:3000/cars';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  // Récupérer toutes les voituresS
+  // Récupérer toutes les voitures
   getAllCars(): Observable<Car[]> {
-    return of(this.cars);
+    return this.http.get<Car[]>(this.apiUrl);
   }
 
   // Récupérer une voiture par son ID
   getCarById(id: number): Observable<Car> {
-    const car = this.cars.find(c => c.id === id);
-    return of(car!);
+    return this.http.get<Car>(`${this.apiUrl}/${id}`);
   }
 
   // Ajouter une nouvelle voiture
   addCar(car: Omit<Car, 'id'>): Observable<Car> {
-    const newCar = { id: this.cars.length + 1, ...car };
-    this.cars.push(newCar);
-    return of(newCar);
+    return this.http.post<Car>(this.apiUrl, car);
   }
 
   // Mettre à jour une voiture
   updateCar(car: Car): Observable<Car> {
-    const index = this.cars.findIndex(c => c.id === car.id);
-    if (index !== -1) {
-      this.cars[index] = car;
-    }
-    return of(car);
+    return this.http.put<Car>(`${this.apiUrl}/${car.id}`, car);
   }
 
   // Supprimer une voiture
   deleteCar(id: number): Observable<void> {
-    this.cars = this.cars.filter(c => c.id !== id);
-    return of();
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
