b2b7153153cb6d41f1dfe17be34981665e345b64
AdelTAHANOUT <adel.tahanout@edu.igensia.com>
2025-05-15 18:01:38 +0200

    Ajout Auth avec Amplify Gen 2

---
+++ src/aws-exports.ts
@@ -0,0 +1,13 @@
+// src/aws-exports.ts
+const awsExports = {
+    Auth: {
+      region: 'eu-west-3',
+      userPoolId: 'eu-west-3_aT44uAuN9',
+      userPoolWebClientId: '401lg051sp2hgudj2lh1us8c6j',
+      identityPoolId: undefined,
+      mandatorySignIn: true
+    }
+  };
+  
+  export default awsExports;
+  
